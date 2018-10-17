95f2a770b97e7e1c92ee7728fb63dce25960e9c6
Bartek <bartek.jastrzebski0010@gmail.com>
2018-10-17 22:31:33 +0200

    Initial Commit

---
+++ booking_system/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  booking_system
+//
+//  Created by Bartosz Jastrzębski on 17/10/2018.
+//  Copyright © 2018 Bartosz Jastrzębski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
